b77369cb416e0f9e3ce7da708febb9ec4b4f6b15
Nguyenthi-8796 <ndthi8796@gmail.com>
2025-11-14 10:24:43 +0700

    Hoàn thành tính năng X

--- src/DAO/DBConnection.java
+++ src/DAO/DBConnection.java
@@ -4,10 +4,38 @@
  */
 package DAO;
 
-/**
- *
- * @author lqanh
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/recipe_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ndtdragon8796";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("OK");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Failed");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Closed");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
