e1ea3000a567afe765e1f3ee60eb7a544edab863
Nguyenthi-8796 <ndthi8796@gmail.com>
2025-11-15 10:29:16 +0700

    fix

--- src/Controller/RecipeController.java
+++ src/Controller/RecipeController.java
@@ -1,13 +1,8 @@
 package Controller;
 
 import DAO.RecipeDAO;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 import model.Recipe;
 import java.util.ArrayList;
-import javax.swing.ImageIcon;
-import model.Ingredient;
 import model.Recipe_Ingredient;
 import model.Step;
 
@@ -15,7 +10,6 @@ public class RecipeController {
 
     private final RecipeDAO recipeDAO;
     private final String[] COLUMN_NAMES = {"ID", "Tên", "Mô tả"};
-
     public RecipeController() {
         this.recipeDAO = new RecipeDAO();
     }
@@ -47,37 +41,4 @@ public class RecipeController {
         }
         return recipe;
     }
-    public Object[] getIngredientTableData(ArrayList<Recipe_Ingredient> ingredients) {
-        String[] INGREDIENT_COLUMNS = {"Ảnh", "Nguyên liệu", "Số lượng", "Đơn vị"};
-        Object[][] data = new Object[ingredients.size()][INGREDIENT_COLUMNS.length];
-
-        for (int i = 0; i < ingredients.size(); i++) {
-            Recipe_Ingredient ri = ingredients.get(i);
-            Ingredient ing = ri.getIngredient();
-
-            data[i][0] = ing.getImageURL();
-            data[i][1] = ing.getName();
-            data[i][2] = ri.getQuantity();
-            data[i][3] = ri.getUnit();
-        }
-        return new Object[]{data, INGREDIENT_COLUMNS};
-    }
-    public ImageIcon getScaledImageIcon(String imagePath, int width, int height) {
-        if (imagePath == null || imagePath.isEmpty()) {
-            return null;
-        }
-        try {
-            File file = new File(imagePath);
-            URL url = file.toURI().toURL();
-            ImageIcon icon = new ImageIcon(url);
-            if (icon.getIconWidth() > 0) {
-                return new ImageIcon(
-                        icon.getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH)
-                );
-            }
-        } catch (MalformedURLException e) {
-        }
-        return null;
-    }
-    
 }
--- src/UI/RecipeDetailFrame.java
+++ src/UI/RecipeDetailFrame.java
@@ -1,24 +1,19 @@
 package UI;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
+import java.awt.Dimension;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableCellRenderer;
-import model.Ingredient;
 import model.Recipe;
-import model.Recipe_Ingredient;
 import model.Step;
 
 public class RecipeDetailFrame extends javax.swing.JFrame {
@@ -53,8 +48,7 @@ public class RecipeDetailFrame extends javax.swing.JFrame {
             ImageIcon icon = new ImageIcon(url);
 
             if (icon.getIconWidth() > 0) {
-                return new ImageIcon(
-                        icon.getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH)
+                return new ImageIcon(icon.getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH)
                 );
             }
         } catch (Exception e) {
@@ -62,34 +56,42 @@ public class RecipeDetailFrame extends javax.swing.JFrame {
         }
         return null;
     }
-    private void displayIngredients(ArrayList<Recipe_Ingredient> ingredients) {
-        String[] columnNames = {"Ảnh", "Nguyên liệu", "Số lượng", "Đơn vị"};
-        Object[][] data = new Object[ingredients.size()][columnNames.length];
-        for (int i = 0; i < ingredients.size(); i++) {
-            Recipe_Ingredient ri = ingredients.get(i);
-            Ingredient ing = ri.getIngredient();
-            data[i][0] = ing.getImageURL();
-            data[i][1] = ing.getName();
-            data[i][2] = ri.getQuantity();
-            data[i][3] = ri.getUnit();
+    void displayIngredients(java.util.ArrayList<model.Recipe_Ingredient> ingredients) {
+        for (model.Recipe_Ingredient ri : ingredients) {
+            model.Ingredient ing = ri.getIngredient();
+            JPanel itemPanel = new JPanel();
+            itemPanel.setLayout(new java.awt.BorderLayout(10, 5)); 
+            itemPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 15, 5, 15));
+
+            JLabel lblImage = new JLabel();
+            lblImage.setIcon(loadImageIcon(ing.getImageURL(), 40, 40));
+            itemPanel.add(lblImage, java.awt.BorderLayout.WEST);
+
+            JTextArea txtDetail = new JTextArea();
+            txtDetail.setEditable(false);
+            txtDetail.setLineWrap(true);
+            txtDetail.setWrapStyleWord(true);
+
+            String detailText = String.format("%s - %d %s",
+                    ing.getName(),
+                    ri.getQuantity(),
+                    ri.getUnit());
+            txtDetail.setText(detailText);
+
+            itemPanel.add(new JScrollPane(txtDetail), java.awt.BorderLayout.CENTER);
+            itemPanel.setPreferredSize(new Dimension(700,100));
+            itemPanel.setMaximumSize(new Dimension(700,100));
+            ingredientsPanel.add(itemPanel);
+            ingredientsPanel.add(javax.swing.Box.createVerticalStrut(5));
         }
-        tblIngredients.setModel(new javax.swing.table.DefaultTableModel(data, columnNames));
-        tblIngredients.getColumnModel().getColumn(0).setCellRenderer(new IngredientImageRenderer());
-        tblIngredients.setRowHeight(50);
-        tblIngredients.getColumnModel().getColumn(0).setPreferredWidth(50);
-        int tableHeight = tblIngredients.getRowCount() * tblIngredients.getRowHeight() + tblIngredients.getTableHeader().getPreferredSize().height;
 
-        ingredientsScrollPane.setPreferredSize(new java.awt.Dimension(
-                ingredientsScrollPane.getWidth(),
-                tableHeight
-        ));
-
+        ingredientsPanel.revalidate();
+        ingredientsPanel.repaint();
         mainContentPanel.revalidate();
         mainContentPanel.repaint();
     }
 
     private void displaySteps(ArrayList<Step> steps) {
-        stepsContainerPanel.setLayout(new BoxLayout(stepsContainerPanel, BoxLayout.Y_AXIS));
         for (Step step : steps) {
             JPanel stepPanel = new JPanel();
             stepPanel.setLayout(new BorderLayout(10, 5));
@@ -97,7 +99,6 @@ public class RecipeDetailFrame extends javax.swing.JFrame {
 
             JLabel lblStepImage = new JLabel();
             lblStepImage.setIcon(loadImageIcon(step.getImageURL(), 100, 100)); 
-            lblStepImage.setHorizontalAlignment(SwingConstants.CENTER);
             stepPanel.add(lblStepImage, BorderLayout.WEST);
 
             JTextArea txtDescription = new JTextArea();
@@ -106,68 +107,65 @@ public class RecipeDetailFrame extends javax.swing.JFrame {
             txtDescription.setWrapStyleWord(true);
             txtDescription.setText(step.getDescription());
             stepPanel.add(new JScrollPane(txtDescription), BorderLayout.CENTER);
+            stepPanel.setPreferredSize(new Dimension(700,200));
+            stepPanel.setMaximumSize(new Dimension(700,200));
             stepsContainerPanel.add(stepPanel);
             stepsContainerPanel.add(Box.createVerticalStrut(10));
         }
         stepsContainerPanel.revalidate();
         stepsContainerPanel.repaint();
-
         mainContentPanel.revalidate();
         mainContentPanel.repaint();
-    }
-
-    class IngredientImageRenderer extends DefaultTableCellRenderer {
-
-        private final JLabel label = new JLabel();
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value,
-                boolean isSelected, boolean hasFocus,
-                int row, int column) {
-            if (value instanceof String) {
-                ImageIcon icon = loadImageIcon((String) value, 40, 40);
-                label.setIcon(icon);
-            } else {
-                label.setIcon(null);
-            }
-            label.setHorizontalAlignment(SwingConstants.CENTER);
-            return label;
-        }
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        mainScrollPane = new javax.swing.JScrollPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
         mainContentPanel = new javax.swing.JPanel();
         lblTitle = new javax.swing.JLabel();
         lblTitle1 = new javax.swing.JLabel();
         txtDescription = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
-        ingredientsScrollPane = new javax.swing.JScrollPane();
-        tblIngredients = new javax.swing.JTable();
+        ingredientsPanel = new javax.swing.JPanel();
         stepsContainerPanel = new javax.swing.JPanel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(700, 700));
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(900, 700));
+
+        jScrollPane1.setOpaque(true);
 
         mainContentPanel.setAlignmentX(0.0F);
         mainContentPanel.setAlignmentY(0.0F);
+        mainContentPanel.setMinimumSize(new java.awt.Dimension(700, 700));
         mainContentPanel.setLayout(new javax.swing.BoxLayout(mainContentPanel, javax.swing.BoxLayout.Y_AXIS));
 
         lblTitle.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblTitle.setText("Tên món ăn");
-        lblTitle.setMaximumSize(new java.awt.Dimension(400, 300));
+        lblTitle.setToolTipText("");
+        lblTitle.setAlignmentX(0.5F);
+        lblTitle.setAlignmentY(0.0F);
+        lblTitle.setOpaque(true);
+        lblTitle.setPreferredSize(new java.awt.Dimension(90, 20));
         mainContentPanel.add(lblTitle);
 
         lblTitle1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblTitle1.setText("Mô tả");
+        lblTitle1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitle1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        lblTitle1.setAlignmentX(0.5F);
+        lblTitle1.setAlignmentY(0.0F);
+        lblTitle1.setAutoscrolls(true);
+        lblTitle1.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
         lblTitle1.setMaximumSize(new java.awt.Dimension(400, 300));
         lblTitle1.setMinimumSize(new java.awt.Dimension(400, 300));
         lblTitle1.setName(""); // NOI18N
         lblTitle1.setOpaque(true);
         lblTitle1.setPreferredSize(new java.awt.Dimension(400, 300));
         mainContentPanel.add(lblTitle1);
+
+        txtDescription.setMaximumSize(new java.awt.Dimension(700, 100));
+        txtDescription.setPreferredSize(new java.awt.Dimension(700, 120));
 
         jTextArea2.setEditable(false);
         jTextArea2.setColumns(20);
@@ -176,53 +174,39 @@ public class RecipeDetailFrame extends javax.swing.JFrame {
 
         mainContentPanel.add(txtDescription);
 
-        ingredientsScrollPane.setMaximumSize(new java.awt.Dimension(9999, 170));
-
-        tblIngredients.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        tblIngredients.setMaximumSize(new java.awt.Dimension(9999, 170));
-        tblIngredients.setRowHeight(50);
-        ingredientsScrollPane.setViewportView(tblIngredients);
-
-        mainContentPanel.add(ingredientsScrollPane);
+        ingredientsPanel.setAlignmentY(0.0F);
+        ingredientsPanel.setMaximumSize(new java.awt.Dimension(9999, 9999));
+        ingredientsPanel.setLayout(new javax.swing.BoxLayout(ingredientsPanel, javax.swing.BoxLayout.Y_AXIS));
+        mainContentPanel.add(ingredientsPanel);
 
         stepsContainerPanel.setMaximumSize(new java.awt.Dimension(9999, 9999));
         stepsContainerPanel.setLayout(new javax.swing.BoxLayout(stepsContainerPanel, javax.swing.BoxLayout.Y_AXIS));
         mainContentPanel.add(stepsContainerPanel);
 
-        mainScrollPane.setViewportView(mainContentPanel);
+        jScrollPane1.setViewportView(mainContentPanel);
 
-        getContentPane().add(mainScrollPane, java.awt.BorderLayout.CENTER);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 900, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane ingredientsScrollPane;
+    private javax.swing.JPanel ingredientsPanel;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea2;
     private javax.swing.JLabel lblTitle;
     private javax.swing.JLabel lblTitle1;
     private javax.swing.JPanel mainContentPanel;
-    private javax.swing.JScrollPane mainScrollPane;
     private javax.swing.JPanel stepsContainerPanel;
-    private javax.swing.JTable tblIngredients;
     private javax.swing.JScrollPane txtDescription;
     // End of variables declaration//GEN-END:variables
 }
